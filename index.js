f48bedbd757ebbb1d81c27f487a9e65f0f7ef900
jperf200588 <jperf200588@gmail.com>
2022-09-11 15:38:21 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+let countHome = 0
+let countEl_Home = document.getElementById("count-el_Home")
+let countGuest = 0
+let countEl_Guest = document.getElementById("count-el_Guest")
+
+
+
+function increment() {
+    countHome += 1
+    countEl_Home.textContent = countHome
+}
+
+function increment2() {
+    countHome += 2
+    countEl_Home.textContent = countHome
+}
+
+function increment3() {
+    countHome += 3
+    countEl_Home.textContent = countHome
+}
+
+function increment4() {
+    countHome -= 1
+    countEl_Home.textContent = countHome
+}
+
+function increment5() {
+    countHome -= 2
+    countEl_Home.textContent = countHome
+}
+
+function increment6() {
+    countHome -= 3
+    countEl_Home.textContent = countHome
+}
+
+
+function increment7() {
+    countGuest += 1
+    countEl_Guest.textContent = countGuest
+}
+
+function increment8() {
+    countGuest += 2
+    countEl_Guest.textContent = countGuest
+}
+
+function increment9() {
+    countGuest += 3
+    countEl_Guest.textContent = countGuest
+}
+
+function increment10() {
+    countGuest -= 1
+    countEl_Guest.textContent = countGuest
+}
+
+function increment11() {
+    countGuest -= 2
+    countEl_Guest.textContent = countGuest
+}
+
+function increment12() {
+    countGuest -= 3
+    countEl_Guest.textContent = countGuest
+}
